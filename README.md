245675dce098430ec979dcccd3b7da782bf4f850
Jose Miguel Bejarano <yo@josemiguel.org>
2016-12-04 14:45:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-react-component-boilerplate [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> A yeoman generator for reusable Reac Components
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-react-component-boilerplate using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-react-component-boilerplate
+```
+
+Then generate your new project:
+
+```bash
+yo react-component-boilerplate
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Jose Miguel Bejarano](josemiguel.org)
+
+
+[npm-image]: https://badge.fury.io/js/generator-react-component-boilerplate.svg
+[npm-url]: https://npmjs.org/package/generator-react-component-boilerplate
+[travis-image]: https://travis-ci.org/xDae/generator-react-component-boilerplate.svg?branch=master
+[travis-url]: https://travis-ci.org/xDae/generator-react-component-boilerplate
+[daviddm-image]: https://david-dm.org/xDae/generator-react-component-boilerplate.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/xDae/generator-react-component-boilerplate
+[coveralls-image]: https://coveralls.io/repos/xDae/generator-react-component-boilerplate/badge.svg
+[coveralls-url]: https://coveralls.io/r/xDae/generator-react-component-boilerplate
---
+++ app/index.js
@@ -0,0 +1,100 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var _ = require('lodash');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the stupendous ' + chalk.red('generator-react-component-boilerplate') + ' generator!'
+    ));
+
+    var prompts = [
+      {
+        type: 'input',
+        name: 'componentName',
+        message: 'First, what is the name of your Component?',
+        default: 'My React Component'
+      },
+      {
+        type: 'input',
+        name: 'version',
+        message: 'The initial version of your Component',
+        default: '0.1.0'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Component description',
+        default: 'My awesome React Component!'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Author',
+        default: ''
+      },
+      {
+        type: 'input',
+        name: 'ghUser',
+        message: 'What is your GitHub Username?',
+        default: ''
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'License',
+        default: 'MIT'
+      }
+    ];
+
+    return this.prompt(prompts)
+      .then(function (props) {
+        // To access props later use this.props.someAnswer;
+        this.props = props;
+      }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copyTpl(
+      this.templatePath('_package.json'), this.destinationPath('package.json'),
+      {name: _.kebabCase(_.deburr(this.props.componentName)),
+        version: this.props.version,
+        description: this.props.description,
+        author: this.props.author,
+        ghUser: this.props.ghUser,
+        license: this.props.license}
+      );
+
+    this.fs.copyTpl(
+      this.templatePath('_README.md'), this.destinationPath('README.md'),
+      {name: this.props.componentName,
+        description: this.props.description}
+    );
+
+    this.fs.copy(
+      this.templatePath('gitignore'), this.destinationPath('.gitignore')
+    );
+    this.fs.copy(
+      this.templatePath('editorconfig'), this.destinationPath('.editorconfig')
+    );
+
+    this.fs.copy(
+      this.templatePath('src/'), this.destinationPath('src/')
+    );
+
+    this.fs.copy(
+      this.templatePath('stories/'), this.destinationPath('stories/')
+    );
+
+    this.fs.copy(
+      this.templatePath('.storybook/'), this.destinationPath('.storybook/')
+    );
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
